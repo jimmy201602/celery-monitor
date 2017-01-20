e685f21d99ffaf3beaca052cd2fa9454f198eb8b
Jimmy <jimmy@Jimmys-MacBook-Air-2.local>
2017-01-21 00:34:41 +0800

    add a front page bug

--- monitor/admin.py
+++ monitor/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
+from djcelery.admin import TaskMonitor
+class mytask(TaskMonitor):
+	list_filter = ('state', 'name', 'tstamp', 'eta', 'worker')

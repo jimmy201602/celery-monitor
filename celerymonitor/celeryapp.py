60158313c005cdcea9a146b524284a68ba231310
jimmy201602 <zhengge2012@gmail.com>
2020-12-14 13:36:06 +0800

    fix celery mutipleprocess bug

--- celerymonitor/celeryapp.py
+++ celerymonitor/celeryapp.py
@@ -4,20 +4,31 @@ from __future__ import absolute_import
 
 import os
 from celery import Celery
+from celery.signals import worker_process_init
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'celerymonitor.settings')
-#Specifying the settings here means the celery command line program will know where your Django project is. 
-#This statement must always appear before the app instance is created, which is what we do next: 
+# Specifying the settings here means the celery command line program will know where your Django project is.
+# This statement must always appear before the app instance is created, which is what we do next:
 from django.conf import settings
-     
+
 app = Celery('celerymonitor')
 app.config_from_object('django.conf:settings')
-#This means that you don’t have to use multiple configuration files, and instead configure Celery directly from the Django settings.
-#You can pass the object directly here, but using a string is better since then the worker doesn’t have to serialize the object.
+# This means that you don’t have to use multiple configuration files, and instead configure Celery directly from the Django settings.
+# You can pass the object directly here, but using a string is better since then the worker doesn’t have to serialize the object.
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
-#With the line above Celery will automatically discover tasks in reusable apps if you define all tasks in a separate tasks.py module.
-#The tasks.py should be in dir which is added to INSTALLED_APP in settings.py. 
-#So you do not have to manually add the individual modules to the CELERY_IMPORT in settings.py.
+# With the line above Celery will automatically discover tasks in reusable apps if you define all tasks in a separate tasks.py module.
+# The tasks.py should be in dir which is added to INSTALLED_APP in settings.py.
+# So you do not have to manually add the individual modules to the CELERY_IMPORT in settings.py.
+from multiprocessing import current_process
 
-@app.task(bind=True)
-def debug_task(self):
-	print('Request: {0!r}'.format(self.request))  #dumps its own request information
+
+@worker_process_init.connect
+def fix_multiprocessing(**kwargs):
+    """
+    This is a known issue with celery. It stems from an issue introduced in the billiard dependency. A work-around is to manually set the _config attribute for the current process. Thanks to user @martinth for the work-around below.
+    https://github.com/celery/celery/issues/1709#issuecomment-122467424
+    https://stackoverflow.com/questions/27904162/using-multiprocessing-pool-from-celery-task-raises-exception
+    """
+    try:
+        current_process()._config
+    except AttributeError:
+        current_process()._config = {'semprefix': '/mp'}
--- celerymonitor/tasks.py
+++ celerymonitor/tasks.py
@@ -4,9 +4,11 @@ import subprocess
 import shlex
 from time import sleep
 
+
 @shared_task(bind=True)
 def debug_task(self):
-    print('Request: {0!r}'.format(self.request))  #dumps its own request information
+    # dumps its own request information
+    print('Request: {0!r}'.format(self.request))
     #print commands.getoutput('ls -l')
     #print commands.getstatusoutput('la -a')
 
@@ -15,36 +17,36 @@ def debug_task(self):
 
     # cmd = ['awk', 'length($0) > 5']
     # p = subprocess.Popen(cmd, stdout=subprocess.PIPE,
-                    # stderr=subprocess.PIPE,
-                    # stdin=subprocess.PIPE)
+    # stderr=subprocess.PIPE,
+    # stdin=subprocess.PIPE)
     # out, err = p.communicate('foo\nfoofoo\n')
     # print out
     # print 'err',err
 
     """https://stackoverflow.com/questions/4760215/running-shell-command-from-python-and-capturing-the-output/4760517"""
-    #real time stdout stderr
+    # real time stdout stderr
     def runs_command(command):
         p = subprocess.Popen(command,
-                    stdout=subprocess.PIPE,
-                    stderr=subprocess.PIPE,
-                    shell=True)
+                             stdout=subprocess.PIPE,
+                             stderr=subprocess.PIPE,
+                             shell=True)
         # Read stdout from subprocess until the buffer is empty !
         for line in iter(p.stdout.readline, b''):
-            if line: # Don't print blank lines
-                print 'stdout',line
+            if line:  # Don't print blank lines
+                print 'stdout', line
                 # yield line
         # This ensures the process has completed, AND sets the 'returncode' attr
-        while p.poll() is None:                                                                                                                                        
-            sleep(.1) #Don't waste CPU-cycles
+        while p.poll() is None:
+            sleep(.1)  # Don't waste CPU-cycles
         # Empty STDERR buffer
         err = p.stderr.read()
         if p.returncode != 0:
-            # The run_command() function is responsible for logging STDERR 
-            print("Error traceback: " + str(err))	
+            # The run_command() function is responsible for logging STDERR
+            print("Error traceback: " + str(err))
         print 11111111
     print runs_command('ping -c 10 baidu.com')
     """https://stackoverflow.com/questions/89228/calling-an-external-command-in-python"""
     """https://stackoverflow.com/questions/4624416/is-there-a-possibility-to-execute-a-python-script-while-being-in-interactive-mod"""
-    
+
     """https://stackoverflow.com/questions/163542/python-how-do-i-pass-a-string-into-subprocess-popen-using-the-stdin-argument"""
     """https://mail.python.org/pipermail/python-list/2015-March/688077.html"""

cea8a924106a490642804eb254fe10236b4edf4a
jimmy201602 <zhengge2012@gmail.com>
2020-12-14 17:15:30 +0800

    add extend api

--- celerymonitor/urls.py
+++ celerymonitor/urls.py
@@ -15,16 +15,25 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 from monitor.views import workers_index
-from django.contrib.auth.views import login,logout
+from django.contrib.auth.views import login, logout
 from rest_framework.authtoken.views import obtain_auth_token
+
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^accounts/login/$', login,{'template_name': 'login.html'},name='login'),
-    url(r'^accounts/logout/$',logout,name='loginout'),
+    url(r'^accounts/login/$', login,
+        {'template_name': 'login.html'}, name='login'),
+    url(r'^accounts/logout/$', logout, name='loginout'),
     url(r'^monitor/', include('monitor.urls')),
-    url(r'^$',workers_index.as_view()),
+    url(r'^$', workers_index.as_view()),
     #url(r'^grappelli/', include('grappelli.urls')),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^api-token-auth/', obtain_auth_token),
 ]
+
+
+try:
+    from .extend_urls import extend_urlpatterns
+    urlpatterns = urlpatterns + extend_urlpatterns
+except ImportError:
+    pass

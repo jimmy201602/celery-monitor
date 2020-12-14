79675054000ab9363cc864edce204c58cd185c2a
jimmy201602 <zhengge2012@gmail.com>
2020-12-14 14:00:42 +0800

    split the private task

--- celerymonitor/tasks.py
+++ celerymonitor/tasks.py
@@ -1,27 +1,15 @@
 from celery import shared_task
-import commands
 import subprocess
-import shlex
 from time import sleep
-
+try:
+    from .extend_tasks import *
+except ImportError:
+    pass
 
 @shared_task(bind=True)
 def debug_task(self):
     # dumps its own request information
     print('Request: {0!r}'.format(self.request))
-    #print commands.getoutput('ls -l')
-    #print commands.getstatusoutput('la -a')
-
-    # status = subprocess.call(['ls', '-l'])
-    # print status
-
-    # cmd = ['awk', 'length($0) > 5']
-    # p = subprocess.Popen(cmd, stdout=subprocess.PIPE,
-    # stderr=subprocess.PIPE,
-    # stdin=subprocess.PIPE)
-    # out, err = p.communicate('foo\nfoofoo\n')
-    # print out
-    # print 'err',err
 
     """https://stackoverflow.com/questions/4760215/running-shell-command-from-python-and-capturing-the-output/4760517"""
     # real time stdout stderr
@@ -43,7 +31,6 @@ def debug_task(self):
         if p.returncode != 0:
             # The run_command() function is responsible for logging STDERR
             print("Error traceback: " + str(err))
-        print 11111111
     print runs_command('ping -c 10 baidu.com')
     """https://stackoverflow.com/questions/89228/calling-an-external-command-in-python"""
     """https://stackoverflow.com/questions/4624416/is-there-a-possibility-to-execute-a-python-script-while-being-in-interactive-mod"""
